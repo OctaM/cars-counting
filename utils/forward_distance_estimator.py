8f3cbb3584dccddf2fb55b07d1b009a7d3429655
OctaM <octa.marian@gmail.com>
2020-10-26 14:09:48 +0200

    fix prediction

--- utils/forward_distance_estimator.py
+++ utils/forward_distance_estimator.py
@@ -32,8 +32,7 @@ class ForwardDistanceEstimator():
         scaled_bbox = self.scaler_x.transform(np.array([x_min, y_min, x_max, y_max]).reshape(1, -1))
         common.set_input_distance_prediction(self.estimation_model, scaled_bbox)
         self.estimation_model.invoke()
-        y_pred = common.output_tensor(self.estimation_model, 0)
-        print(y_pred.shape)
+        y_pred = [common.output_tensor(self.estimation_model, 0)]
         # y_pred = self.estimation_model.predict(scaled_bbox)
         dist = self.scaler_y.inverse_transform(y_pred)
         return dist
